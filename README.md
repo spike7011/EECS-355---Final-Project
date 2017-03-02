999249c36d96f70edc1549bf3816531c1a7f3be7
spike7011 <jasonglass2017@u.northwestern.edu>
2017-03-02 16:05:42 -0600

    "changes to readme/package"

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Use this file to manage all I/O for entities.
 Paste your entity descriptions here:
+
+top_tank: process to generate x-coordinate values for tanks with
+adjustable speed and bounded coordinates.
--- tank_package.vhd
+++ tank_package.vhd
@@ -6,6 +6,27 @@ use IEEE.std_logic_1164.all;
 package tank_package is
 	constant BOUNDING_BOX_CORNERS : natural := 4;
 	type bounding_box is array(0 to BOUNDING_BOX_CORNERS - 1) of integer; -- (x_left, x_right, y_top, y_bottom)
+
+
+	component top_tank is
+		port(
+		clk : in std_logic;
+		rst : in boolean;
+		x_in : in integer;
+		SPEED : in integer;
+		x_out : out integer);
+	end component;
+
+
+
+
+
+
+
+
+
+
+
 end package tank_package;
 package body tank_package is
 
