a409d0839ea1c881c4dee21f9c063cf030f46a0b
aryder1994 <aryder1994@gmail.com>
2017-03-02 16:23:24 -0600

    Final collision_detector

--- collision_detector.vhd
+++ collision_detector.vhd
@@ -13,40 +13,40 @@ entity collision_detector is
 end entity collision_detector;
 
 architecture behavioral of collision_detector is
-
+	signal overlap_left, overlap_right, overlap_top, overlap_bottom : std_logic;
 begin
-	check_collision: process (bounding_box_a, bounding_box_b)
+	check_collision: process (bounding_box_a, bounding_box_b, overlap_left, overlap_right, overlap_top, overlap_bottom)
 	begin
 	
 		-- initialize to prevent latches
 		overlap_left <= '0';
 		overlap_right <= '0';
 		overlap_top <= '0';
-		overlap_top <= '0';
+		overlap_bottom <= '0';
 		collision <= '0';
 		
 		-- check if a_left_x is between b_left_x and b_right_x
-		if (bounding_box_a(0) >= bounding_box_b(0)) and (bounding_box_a(0) <= bounding_box_b(1)) then
+		if ((bounding_box_a(0) >= bounding_box_b(0)) and (bounding_box_a(0) <= bounding_box_b(1))) then
 			overlap_left <= '1';
 		end if;
 			
 		-- check if a_right_x is between b_left_x and b_right_x
-		if (bounding_box_a(1) >= bounding_box_b(0)) and (bounding_box_a(1) <= bounding_box_b(1)) then
+		if ((bounding_box_a(1) >= bounding_box_b(0)) and (bounding_box_a(1) <= bounding_box_b(1))) then
 			overlap_right <= '1';
 		end if;
 			
 		-- check if a_top_y is between b_top_y and b_bottom_y
-		if (bounding_box_a(2) >= bounding_box_b(2)) and (bounding_box_a(2) <= bounding_box_b(3)) then
+		if ((bounding_box_a(2) >= bounding_box_b(2)) and (bounding_box_a(2) <= bounding_box_b(3))) then
 			overlap_top <= '1';
 		end if;
 	
 		-- check if a_bottom_y is between b_top_y and b_bottom_y
-		if (bounding_box_a(3) >= bounding_box_b(2)) and (bounding_box_a(3) <= bounding_box_b(3)) then
+		if ((bounding_box_a(3) >= bounding_box_b(2)) and (bounding_box_a(3) <= bounding_box_b(3))) then
 			overlap_bottom <= '1';
 		end if;
 		
 		-- check for collision
-		if (overlap_left or overlap_right) and (overlap_top or overlap_bottom) then
+		if (((overlap_left or overlap_right) and (overlap_top or overlap_bottom)) = '1') then
 			collision <= '1';
 		end if;
 	end process check_collision;
