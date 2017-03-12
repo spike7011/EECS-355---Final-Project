7e20043030e3bb7fc4d401be15a97a87af57c046
aryder1994 <aryder1994@gmail.com>
2017-03-12 17:32:53 -0500

    Change to tank_clock

--- tank_clock.vhd
+++ tank_clock.vhd
@@ -21,14 +21,14 @@ begin
 		counter := counter;
 		if (rising_edge(clk)) then
 			counter := counter + 1;
-			if (counter = (500000 / speed)) then
+			if ((counter >= (500000 / speed)) and (counter < (1000000 / speed))) then
 				tank_clk <= '1';
-			elsif (counter = (1000000 / speed)) then
+			elsif (counter >= (1000000 / speed)) then
 				counter := 0;
 				tank_clk <= '0';
 			end if;
 		end if;
-		
+
 		if (reset = '1') then
 			counter := 0;
 			tank_clk <= '0';
